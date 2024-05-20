7ee806fc99f93b1ca7c2629868283b60f1b0218e
Ryo <134446043+Ryo010@users.noreply.github.com>
2024-05-20 20:05:44 +0530

    finally its done

---
+++ buttons/static.js
@@ -0,0 +1,12 @@
+function link() {
+    window.open("./static website/index.html", "_blank");
+}
+function github() {
+    window.open("https://github.com/Ryo010", "_blank");
+}
+function insta() {
+    window.open("https://www.instagram.com/hoshiyomiryo/", "_blank");
+}
+function twitter() {
+    window.open("https://x.com/Ry0ko11", "_blank");
+}
