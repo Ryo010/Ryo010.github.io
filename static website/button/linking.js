ace6facb28ea920ce26277f9e7038670fb8587f8
Ryo <134446043+Ryo010@users.noreply.github.com>
2024-06-28 12:15:06 +0530

    i am so back

---
+++ static website/button/linking.js
@@ -0,0 +1,15 @@
+function suisei() {
+    window.open("https://hololive.hololivepro.com/en/talents/hoshimachi-suisei/", "_blank");
+}
+function yuuri() {
+    window.open("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Yuuri_(singer-songwriter)&ved=2ahUKEwic-oSvysaFAxUQS2cHHYHzC-AQFnoECGAQAQ&usg=AOvVaw3oOQzZ4C3E6bOh4xnthpzD", "_blank");
+}
+function tk() {
+    window.open("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Tatsuya_Kitani&ved=2ahUKEwjQ3_KgysaFAxVNcWwGHVxxC2YQFnoECF4QAQ&usg=AOvVaw2lI1MTVUMpcjdJdIo6rIXA", "_blank");
+}
+function Vaundy() {
+    window.open("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Vaundy&ved=2ahUKEwjB18OKx8aFAxXkbmwGHcGsARsQFnoECE0QAQ&usg=AOvVaw19yAhCINWEDHPB5RQak9ML", "_blank");
+}
+function Eve() {
+    window.open("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Eve_(Japanese_singer)&ved=2ahUKEwjQ4Zf4xsaFAxVASGcHHU0XA4gQFnoECDcQAQ&usg=AOvVaw2nJ5ZbvmZ5Q6cCbSCGbjfH", "_blank");
+}
